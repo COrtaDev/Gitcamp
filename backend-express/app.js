45a7178d9f586edf12acf33f8705d29eac1c89f3
Conrad Orta <corta.dev@gmail.com>
2020-08-04 14:31:48 -0400

    Add stucture to login

--- backend-express/app.js
+++ backend-express/app.js
@@ -18,19 +18,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.use(function(_req, _res, next) {
-  next(createError(404));
+app.use(function (_req, _res, next) {
+    next(createError(404));
 });
 
-app.use(function(err, _req, res, _next) {
-  res.status(err.status || 500);
-  if (err.status === 401) {
-    res.set('WWW-Authenticate', 'Bearer');
-  }
-  res.json({
-    message: err.message,
-    error: JSON.parse(JSON.stringify(err)),
-  });
+app.use(function (err, _req, res, _next) {
+    res.status(err.status || 500);
+    if (err.status === 401) {
+        res.set('WWW-Authenticate', 'Bearer');
+    }
+    res.json({
+        message: err.message,
+        error: JSON.parse(JSON.stringify(err)),
+    });
 });
 
 module.exports = app;
